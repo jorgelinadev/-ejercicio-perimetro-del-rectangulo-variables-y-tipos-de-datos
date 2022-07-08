dbfdf5ef33c3a3ec412e3a0d4d757a253915c47a
jorgelinadev <jorgelinalberti@gmail.com>
2022-07-08 10:35:00 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+//alert ("Mi archivo está bien linkeado")
+
+const ladoMayor = prompt ('Ingresa el valor del lado mayor de tu rectángulo en cm');
+const ladoMenor = prompt ('Ingresa el valor del lado menor de tu rectángulo en cm')
+
+const resultado = (ladoMayor * 2 ) + (ladoMenor * 2);
+
+alert ('El perímetro de tu rectángulo es de '+ resultado + ' centímetros.');
+
+console.log (alert);
+
